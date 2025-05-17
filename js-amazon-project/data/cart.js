b7f778008eb7e505e2d3d34bd0ab680c711d80ac
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-17 22:37:04 +0800

    Save data and generate HTML for delivery options

--- js-amazon-project/data/cart.js
+++ js-amazon-project/data/cart.js
@@ -1,10 +1,12 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    deliveryOptionId: '1',
     quantity: 2
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    deliveryOptionId: '2',
     quantity: 1
   }
 ];
@@ -25,7 +27,8 @@ export function addToCart(productId) {
   else {
     cart.push({
       productId,
-      quantity
+      quantity,
+      deliveryOptionId: '1'
     });
   }
   console.log(cart);
