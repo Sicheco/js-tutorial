e5b72452bff4a37b44d9f4236929c0ad04b9da3e
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-18 23:06:47 +0800

    Save data and generate HTML for order summary

--- js-amazon-project/scripts/checkout/orderSummary.js
+++ js-amazon-project/scripts/checkout/orderSummary.js
@@ -1,6 +1,6 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateCartQuantity, handleUpdateQuantity, updateDeliveryOption, saveToStorage } from "../../data/cart.js";
-import { products } from "../../data/products.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { products, getProduct } from "../../data/products.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
@@ -11,22 +11,11 @@ export function renderOrderSummary() {
 
   cart.forEach((cartItem) => {
     const productId = cartItem.productId;
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if (productId === product.id) {
-        matchingProduct = product;
-      }
-    });
-
-    let deliveryOption;
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
+
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
