ebd950f26e56001998c54fc0baabac3ec7f30b65
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-20 02:28:40 +0800

    Add jasmine testing framewwork

---
+++ js-amazon-project/tests/moneyTest.js
@@ -0,0 +1,9 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+const money = [2000.5, 1082, -2983, 0, 398, 1092.9, NaN, 5 + 2, 2000.4];
+
+let testItem;
+money.forEach((item) => {
+  testItem = formatCurrency(item);
+  console.log(`Item: ${item}, testItem: ${testItem}`);
+});
