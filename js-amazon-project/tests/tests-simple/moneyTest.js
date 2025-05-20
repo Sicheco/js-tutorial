baf26e3060e746702c041b4c93198f61367347ec
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-21 04:32:14 +0800

    Organized test folder

--- js-amazon-project/tests/tests-simple/moneyTest.js
+++ js-amazon-project/tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 const money = [2000.5, 1082, -2983, 0, 398, 1092.9, NaN, 5 + 2, 2000.4];
 
