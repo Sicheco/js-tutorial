ffeab4423c735c95f35ce2ad8fd8c3d370ec2278
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-27 05:55:02 +0800

    Add backend practice file

--- js-amazon-project/scripts/checkout.js
+++ js-amazon-project/scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-class.js'
 //import { cars } from '../data/car.js';
+import '../data/backend-practice.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
