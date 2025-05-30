595c254b5a0474f2e3dbd2ecaa3417d53156d770
Vincent Sicheco <vincentrhiansichecowork@gmail.com>
2025-05-30 11:30:00 +0800

    Learn error handling

--- js-amazon-project/data/products.js
+++ js-amazon-project/data/products.js
@@ -97,7 +97,11 @@ export function loadProductsFetch() {
       }
     });
     console.log('load products');
-  })
+  })/*.catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+  */
+
   return promise;
 }
 
@@ -120,6 +124,10 @@ export function loadProducts(fun) {
     console.log('load products');
     fun();
   });
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try againlater.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
--- js-amazon-project/scripts/checkout.js
+++ js-amazon-project/scripts/checkout.js
@@ -8,14 +8,22 @@ import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadCart } from "../data/cart.js";
 
 async function loadPage() {
+  try {
+    //throw 'error1';
+    await loadProductsFetch();
 
-  await loadProductsFetch();
-
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
+    await new Promise((resolve, reject) => {
+      //throw 'error2';
+      loadCart(() => {
+        //reject('error3');
+        resolve();
+      });
     });
-  });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+    console.log(error);
+  }
 
   renderCheckoutHeader();
   renderOrderSummary();
